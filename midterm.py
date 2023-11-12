5364e0529e3ed48341acdc4286ed613710f00da8
Alaa Albast <alaabast8803@gmail.com>
2023-11-12 08:36:01 +0200

    c5

--- midterm.py
+++ midterm.py
@@ -4,20 +4,26 @@ Created on Sun Nov 12 07:57:37 2023
 
 @author: Lenovo
 """
-
+import webbrowser
+       
 def main():
-    print("WELCOME!")
-    while(1):
-        print("Choose a option from the list :\n ")
-        print("1. Open Tab \n2. Close Tab \n3. Switch Tab \n4. Display All Tabs \n5. Open Nested Tab \n6. Clear All Tabs \n7. Save Tabs \n8. Import Tabs\n9. Exit")
-        x=int(input("Your choose:"))
-        if x==1:
-            openTab()
-            
-            
-            
+  print("WELCOME!")
+  while True:
+    print("\nChoose a option from the list :\n ")
+    print("1. Open Tab \n2. Close Tab \n3. Switch Tab \n4. Display All Tabs \n5. Open Nested Tab \n6. Clear All Tabs \n7. Save Tabs \n8. Import Tabs\n9. Exit")
+    x = int(input("Your choose:"))
+    if x == 1:
+        title=input("Enter the title of the website")                
+        link = input("Enter the link of the website: ")
+        openNewTab(link)
 
-main()
 
-openTab():
+
+def openNewTab(link):      
+    webbrowser.open(link)
     
+    
+    
+    
+    
+main()

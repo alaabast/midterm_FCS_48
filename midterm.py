a09ac0b7ba3c8bc17b1a2bb4b4a31f471ea94dfc
Alaa Albast <alaabast8803@gmail.com>
2023-11-12 08:11:01 +0200

    c4

--- midterm.py
+++ midterm.py
@@ -11,4 +11,13 @@ def main():
         print("Choose a option from the list :\n ")
         print("1. Open Tab \n2. Close Tab \n3. Switch Tab \n4. Display All Tabs \n5. Open Nested Tab \n6. Clear All Tabs \n7. Save Tabs \n8. Import Tabs\n9. Exit")
         x=int(input("Your choose:"))
-        
+        if x==1:
+            openTab()
+            
+            
+            
+
+main()
+
+openTab():
+    

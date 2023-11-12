2a9521f695a26cdb7cf66321bf7b182c9108d583
Alaa Albast <alaabast8803@gmail.com>
2023-11-12 08:01:56 +0200

    c1

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-
-        myFrame f=new myFrame();
-        f.setVisible(true);
-        f.setSize(600,350);
-    }
-}
---
+++ midterm.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 12 07:57:37 2023
+
+@author: Lenovo
+"""
+

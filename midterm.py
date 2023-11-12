b3b5823d9ed2ad419e7e1d43e6802dc294af2c70
Alaa Albast <alaabast8803@gmail.com>
2023-11-12 09:02:57 +0200

    create the dic.

--- midterm.py
+++ midterm.py
@@ -7,17 +7,19 @@ Created on Sun Nov 12 07:57:37 2023
 import webbrowser
        
 def main():
-  print("WELCOME!")
-  while True:
-    print("\nChoose a option from the list :\n ")
-    print("1. Open Tab \n2. Close Tab \n3. Switch Tab \n4. Display All Tabs \n5. Open Nested Tab \n6. Clear All Tabs \n7. Save Tabs \n8. Import Tabs\n9. Exit")
-    x = int(input("Your choose:"))
-    if x == 1:
-        title=input("Enter the title of the website")                
-        link = input("Enter the link of the website: ")
-        openNewTab(link)
-    if x==2:
-        index=input("Enter the title of the website :")
+    website_dict = {}
+    print("WELCOME!")
+    while True:
+        print("\nChoose a option from the list :\n ")
+        print("1. Open Tab \n2. Close Tab \n3. Switch Tab \n4. Display All Tabs \n5. Open Nested Tab \n6. Clear All Tabs \n7. Save Tabs \n8. Import Tabs\n9. Exit")
+        x = int(input("Your choose:"))
+        if x == 1:
+            title=input("Enter the title of the website")                
+            link = input("Enter the link of the website: ")
+            website_dict[title] =link
+            openNewTab(link)
+        if x==2:
+            index=input("Enter the title of the website :")
         
 
 

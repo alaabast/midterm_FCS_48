b3dd2c6caaedad411be3ae5e20195b5ed3be7a55
Alaa Albast <alaabast8803@gmail.com>
2023-11-12 10:44:31 +0200

    complete part 2

--- midterm.py
+++ midterm.py
@@ -1,34 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 12 07:57:37 2023
-
-@author: Lenovo
-"""
 import webbrowser
-       
+
 def main():
     website_dict = {}
+
     print("WELCOME!")
     while True:
-        print("\nChoose a option from the list :\n ")
+        print("\nChoose an option from the list :\n ")
         print("1. Open Tab \n2. Close Tab \n3. Switch Tab \n4. Display All Tabs \n5. Open Nested Tab \n6. Clear All Tabs \n7. Save Tabs \n8. Import Tabs\n9. Exit")
         x = int(input("Your choose:"))
+
         if x == 1:
-            title=input("Enter the title of the website")                
+            title = input("Enter the title of the website:")
             link = input("Enter the link of the website: ")
-            website_dict[title] =link
+            website_dict[title] = link
+
             openNewTab(link)
-        if x==2:
-            index=input("Enter the title of the website :")
-        
 
+        if x == 2:
+            index = input("Enter the title of the website :")
+            if index in website_dict:
+                webbrowser.get(website_dict[index]).close()
+            else:
+                print("this tab is not availble\n Closing the last tab...")
+                l = list(website_dict);
+                last_link = l[-1]
+                webbrowser.get(last_link).close()
+       if x==3:
+           
 
+def openNewTab(link):
+    webbrowser.open(link)
 
-def openNewTab(link):      
-    webbrowser.open(link) #the source: https://blog.finxter.com/how-to-open-a-url-in-your-browser-from-a-python-script/
-    
-    
-    
-    
-    
 main()

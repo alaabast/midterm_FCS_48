f9afaf84ead33bd1db54cc5e7984b6f6e8e8e18b
Alaa Albast <alaabast8803@gmail.com>
2023-11-12 08:08:54 +0200

    c3

--- midterm.py
+++ midterm.py
@@ -9,3 +9,6 @@ def main():
     print("WELCOME!")
     while(1):
         print("Choose a option from the list :\n ")
+        print("1. Open Tab \n2. Close Tab \n3. Switch Tab \n4. Display All Tabs \n5. Open Nested Tab \n6. Clear All Tabs \n7. Save Tabs \n8. Import Tabs\n9. Exit")
+        x=int(input("Your choose:"))
+        
